0cc55918f22eec54d87a7268f77f0cd08b1bb6d2
Didion, John (NIH/NHGRI) [F] <john.didion@nih.gov>
2016-08-06 16:25:55 -0400

    bump version

--- atropos/__init__.py
+++ atropos/__init__.py
@@ -2,7 +2,7 @@
 from __future__ import print_function, division, absolute_import
 import sys
 
-__version__ = '1.0.4'
+__version__ = '1.0.5'
 
 def check_importability():  # pragma: no cover
     try:
