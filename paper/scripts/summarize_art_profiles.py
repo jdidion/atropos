9128c0df2923db2a05a5e3c356d3aed22f2db2f0
Didion, John (NIH/NHGRI) [F] <john.didion@nih.gov>
2016-08-08 11:00:02 -0400

    add adapter index to output

--- paper/scripts/summarize_art_profiles.py
+++ paper/scripts/summarize_art_profiles.py
@@ -25,7 +25,7 @@ class fileoutput(object):
         if self.close:
             self.fh.close()
 
-def summarize_art_profile(p, w, adapter):
+def summarize_art_profile(p, w, i, adapter):
     while True:
         line = next(p)
         if line.startswith('#') or line.startswith('@'): continue
@@ -65,7 +65,7 @@ def summarize_art_profile(p, w, adapter):
             num_reads_mismatch += 1
             total_edit_dist += editdistance.eval(r1,r2)
     
-    w.writeline((
+    w.writerow((i,
         num_adapters, num_reads_mismatch, total_reads,
         total_edit_dist, total_ref_bp,
         total_adapter_edit_dist, total_adapter_bp
@@ -81,13 +81,13 @@ def main():
     
     with open(args.profile1, 'rt') as p1, open(args.profile2, 'rt') as p2, fileoutput(args.output) as o:
         w = csv.writer(o, separator="\t")
-        w.writeline((
+        w.writeline(('adapter',
             'num_adapters', 'num_reads_mismatch', 'total_reads',
             'total_edit_dist', 'total_ref_bp',
             'total_adapter_edit_dist', 'total_adapter_bp'
         ))
-        summarize_art_profile(p1, w, args.adapters[0])
-        summarize_art_profile(p2, w, args.adapters[1])
+        summarize_art_profile(p1, w, 1, args.adapters[0])
+        summarize_art_profile(p2, w, 2, args.adapters[1])
 
 if __name__ == "__main__":
     main()
