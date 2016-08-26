a4ef7bb14979cb2587ac5e29f16e21ee56429efa
Didion, John (NIH/NHGRI) [F] <john.didion@nih.gov>
2016-08-26 13:49:01 -0400

    bugfixes

--- paper/scripts/estimate_real_data_metrics.py
+++ paper/scripts/estimate_real_data_metrics.py
@@ -8,8 +8,8 @@ ADAPTER1 = "GATCGGAAGAGCACACGTCTGAACTCCAGTCACCAGATCATCTCGTATGCCGTCTTCTGCTTG" # T
 ADAPTER2 = "AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGTAGATCTCGGTGGTCGCCGTATCATT" # TruSeq universal
 
 def estimate_metrics(fq1, fq2, adapter1, adapter2):
-    adapter1_re = re.compile(adapter1 + '(A{8}|A+$)')
-    adapter2_re = re.compile(adapter2 + '(A{8}|A+$)')
+    adapter1_re = re.compile(adapter1 + '(A{8}|A*$)')
+    adapter2_re = re.compile(adapter2 + '(A{8}|A*$)')
     adapter_len1 = len(adapter1)
     adapter_len2 = len(adapter2)
     
@@ -24,14 +24,14 @@ def estimate_metrics(fq1, fq2, adapter1, adapter2):
         q = ord(qchar) - 33
         return 10**(-q/10)
         
-    for read1, read2 in tqdm.tqdm(zip(fq_iterator(fq1), fq_iterator(fq2))):
-        cum_qual += sum(qual2prob(qchar) for qchar in read1[2])
-        cum_qual += sum(qual2prob(qchar) for qchar in read2[2])
-        cum_len += len(read1[2]) + len(read2[2])
-        if read1[1].endswith(adapter1):
+    for read1, read2 in tqdm.tqdm(zip(fq_iterator(fq1, 1), fq_iterator(fq2, 2))):
+        cum_qual += sum(qual2prob(qchar) for qchar in read1[3])
+        cum_qual += sum(qual2prob(qchar) for qchar in read2[3])
+        cum_len += len(read1[2]) + len(read2[3])
+        if adapter1_re.search(read1[2]):
             num_adapters1 += 1
             adapter_bases1 += adapter_len1
-        if read1[2].endswith(adapter2):
+        if adapter2_re.search(read2[2]):
             num_adapters2 += 1
             adapter_bases2 += adapter_len2
     
