16089ea3263590c07d2bc5759c6f090fc22a88ce
Didion, John (NIH/NHGRI) [F] <john.didion@nih.gov>
2016-08-26 16:59:15 -0400

    finish summary script

--- paper/scripts/summarize_real_trimming_accuracy.py
+++ paper/scripts/summarize_real_trimming_accuracy.py
@@ -3,7 +3,7 @@ import csv
 
 nuc = ('A','C','G','T','N')
 
-def summarize(bam, w, adapter1, adapter2, n_bases=20):
+def summarize(bam, w, adapter1, adapter2, mapqs, n_bases=20):
     # position base histograms
     read1_hist = [
         dict((base, [0] * n_bases) for base in ('A','C','G','T','N'))
@@ -13,6 +13,8 @@ def summarize(bam, w, adapter1, adapter2, n_bases=20):
         dict((base, [0] * n_bases) for base in ('A','C','G','T','N'))
         for i in range(2)
     ]
+    summaries = dict((q, [0, 0, 0, 0]))
+    mapqs = sorted(mapqs, reverse=True)
     
     def summarize_read(r, hist):
         for i, b in enuermate_range(r.query_sequence.upper(), 0, n_bases):
@@ -31,10 +33,19 @@ def summarize(bam, w, adapter1, adapter2, n_bases=20):
     for r1, r2 in bam:
         r1_summary = summarize_read(r1, read1_hist)
         r2_summary = summarize_read(r2, read2_hist)
-        assert r1_summary[0] == r2_summary[0]
-        w.writerow((r1.query_name, r1_summary[0]) + r1_summary[1:] + r2_summary[1:])
+        proper = r1_summary[0]
+        assert proper == r2_summary[0]
+        w.writerow((r1.query_name, proper) + r1_summary[1:] + r2_summary[1:])
+        
+        minq = min(r1_summary[2], r2_summary[2])
+        for q in mapqs:
+            if minq >= q:
+                summaries[q][0] += 1
+                summaries[q][1] += int(proper)
+                summaries[q][2] += r1_summary[3] - r1_summary[4]
+                summaries[q][3] += r2_summary[3] - r2_summary[4]
     
-    return (read1_hist, read2_hist)
+    return (summaries, (read1_hist, read2_hist))
 
 def main():
     parser = argparse.ArgumentParser()
@@ -42,8 +53,11 @@ def main():
     parser.add_argument("-A", "--adapter2", default=ADAPTER2)
     parser.add_argument("-b", "--bam")
     parser.add_argument("-o", "--output", default="-")
+    parser.add_argument("-s", "--summary", default="-")
     paraser.add_argument("-h", "--hist", default="trimmed_hists.txt")
     args = parser.parse_args()
+    
+    mapqs = (0,10,20,30,40)
     
     with BAMReader(args.bam) as bam, open_output(args.output) as o:
         w = csv.writer(o, delimiter="\t")
@@ -52,7 +66,12 @@ def main():
             for field in ('mapped', 'mapq', 'trimmed_len', 'clipped_len')
             for i in (1,2)
         ))
-        hists = summarize(bam, w, args.adapter1, args.adapter2)
+        summaries, hists = summarize(bam, w, args.adapter1, args.adapter2, mapqs)
+    
+    with open_output(args.summary) as s:
+        w = csv.writer(s, delimiter="\t")
+        for q in mapqs:
+            w.writerow([q] + summaries[q])
     
     with open_output(args.hist) as h:
         w = csv.writer(h, delimiter="\t")
