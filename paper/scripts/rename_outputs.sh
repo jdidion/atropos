e13634cb817657a0761620ebcf8a3b4dddfc03c6
Mr John Paul Didion <didionjp@khead01.gc.nih.gov>
2016-08-30 16:19:32 -0400

    fix rename script

--- paper/scripts/rename_outputs.sh
+++ paper/scripts/rename_outputs.sh
@@ -1,11 +1,11 @@
 # Rename all fastq files to have a .[12].fq.gz extension
-for file in results/skewer*1.fastq.gz
+for file in results/skewer*trimmed-pair1.fastq.gz
 do
-    base="${file%%1.*}"
+    base="${file%%-trimmed-pair1.*}"
     mv $file $base.1.fq.gz
 done
 for file in results/skewer*2.fastq.gz
 do
-    base="${file%%2.*}"
+    base="${file%%-trimmed-pair2.*}"
     mv $file $base.2.fq.gz
 done
