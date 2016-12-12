7fbdab84391033effe5b8c993a970b3cd70c2628
Didion, John (NIH/NHGRI) [F] <john.didion@nih.gov>
2016-12-12 16:17:28 -0500

    update plotting script

--- paper/scripts/plot_real_results.R
+++ paper/scripts/plot_real_results.R
@@ -135,11 +135,16 @@ process.rna <- function(rna) {
     rna.tab.tidy
 }
 
+plot.rna.data <- function(rna.data) {
+    ggplot(rna.data, aes(x=MAPQ, y=log10(delta), col=Program, group=Program)) +
+        geom_line() +
+        geom_point() +
+        xlab('Mapping Quality Score (MAPQ) Cutoff') +
+        ylab('Log10(Difference versus\nUntrimmed Reads)')
+}
+
 rna <- read_tsv('rnaseq_results.txt')
 rna.data <- process.rna(rna)
 pdf('Figure3.pdf')
-ggplot(rna.data, aes(x=MAPQ, y=log10(delta), col=Program, group=Program)) +
-    geom_line() + geom_point() +
-    xlab('Mapping Quality Score (MAPQ) Cutoff') +
-    ylab('Log10(Difference versus\nUntrimmed Reads)')
+plot.rna.data(rna.data)
 dev.off()
