08aad07188a92fc6ad0048a456a088d829c8904b
jdidion <github@didion.net>
2019-11-20 13:23:05 -0500

    Set minimum version to 3.4.5

--- setup.py
+++ setup.py
@@ -176,7 +176,6 @@ setup(
         "License :: Public Domain",
         "Natural Language :: English",
         "Programming Language :: Cython",
-        "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6"
